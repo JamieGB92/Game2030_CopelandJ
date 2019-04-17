d581481c021948a0b47df4f1fe7407314b4a86d9
JamieGB92 <jamie.g.copeland@gmail.com>
2019-04-16 22:43:34 -0400

    fixed some things

---
+++ Source/Game2030_CopelandJ/StealthGameHighScoreSaves.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "StealthGameHighScoreSaves.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME2030_COPELANDJ_API UStealthGameHighScoreSaves : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
